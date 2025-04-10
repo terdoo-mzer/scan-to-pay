52b1f9839dd3b0e2d79de2cf3feb7c809175765b
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2025-04-10 11:59:48 +0100

    alert product

--- src/views/ScannerView.vue
+++ src/views/ScannerView.vue
@@ -182,7 +182,7 @@ const checkProductInDB = async (productCode) => {
       },
     })
     const data = await response.json()
-    alert('Product Data: ', data)
+    alert('Product Data Status: ', data.status)
     return data
   } catch (error) {
     console.error('Error fetching product data:', error)
