cea0e1a3af8cc46eb8928d55a203f6e651162d56
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2025-04-22 20:18:03 +0100

    Fix Toast Component importation in Cart view

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -35,7 +35,7 @@
       <pre>{{ apiRes.message }}</pre>
     </div>
   </div>
-  <toastComponent
+  <ToastComponent
     v-model:toastMessage="toastMessage"
     v-model:toastType="toastType"
     v-model:toastIsVisible="toastIsVisible"
@@ -44,7 +44,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import toastComponent from '@/components/toastComponent.vue'
+import ToastComponent from '@/components/ToastComponent.vue'
 
 const deferredPrompt = ref(null)
 
